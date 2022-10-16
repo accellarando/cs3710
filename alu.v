5639b79726dbee5af28a62343d25cc083e1f78bb
bsitepie <77819227+bsitepie@users.noreply.github.com>
2022-10-15 20:20:05 -0600

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,30 +1,31 @@
 module alu #(parameter WIDTH = 16)
             (input      [WIDTH-9:0] aluIn1, aluIn2, 
              input      [3:0]       aluCont, 
-				 input		cin;
-             output reg [WIDTH-1:0] aluout)
-				 output cOut;
+	     input	cin,
+             output reg [WIDTH-1:0] aluout
+	     output cOut;
+	    );
 
    wire     [WIDTH-9:0] b2, sum, slt, shift;
-	reg [15:0] alu_Result
+   reg [15:0] aluResult;
 
-   assign b2 = aluCont[2] ? ~aluInt2:aluInt2; 
-   assign sum = a + b2 + aluCont[3];
-	//inplement shift left and shift rigth inside ALU
-	//assign shift_left=  
+	assign b2 = aluCont[2] ? ~aluIn1 : aluIn2; 
+   assign sum = aluIn1 + b2 + aluCont[3];
+	//implement shifter inside ALU 
 	
-	assign alu_out = alu_Result;
+   assign aluOut = aluResult;
 	
    // slt should be 1 if most significant bit of sum is 1
    assign slt = sum[WIDTH-1];
 
    always@(*) begin
       case(alucont[2:0])
-         3'b000: aluOut <= a & b;  // logical AND
-         3'b001: aluOut <= a | b;  // logical OR
-         3'b010: aluOut <= sum;    // Add/Sub
-         3'b011: aluOut <= slt;    // set-less-than
-			3'b100: aluOut <= shift; 	// shift left/shift right
-			default aluOut <= a & b
-			
-      endcase
+         3'b000: aluResult <= a & b;  // logical AND
+         3'b001: aluResult <= a | b;  // logical OR
+         3'b010: aluResult <= sum;    // Add/Sub
+         3'b011: aluResult <= slt;    // set-less-than
+	 3'b100: aluResult <= shift; 	// shift left/shift right
+	 default aluResult <= aluIn1 & aluIn2;		
+	 endcase
+   end
+endmodule
