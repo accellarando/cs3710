8feec8df985ea3fec119043970dc773233ca7292
Jacamarsh <112056604+Jacamarsh@users.noreply.github.com>
2022-10-17 00:06:53 -0600

    Ella, I updated stuff, see comments in code - Jack

--- cpu.v
+++ cpu.v
@@ -8,8 +8,11 @@ module cpu #(parameter SIZE=16, NUMREGS=16)
 		
 	wire[SIZE-1:0] pcIn, pcOut, srcOut, dstOut, immOut, 
 		d1, d2, aluIn1, aluIn2, aluOut, shifterOut, aluOutShifted, aluFinal;
-	wire ctrlAlu1, ctrlAlu2, carryIn, carryOut;
-	wire[4:0] conds;
+	wire ctrlAlu1, ctrlAlu2, cIn, en; // changed carryIn to cIn, added en, removed "carryOut"
+	
+	// changed [4:0] conds to 2 groups: [1:0] cond_group1, [2:0] cond_group2
+	wire[1:0] cond_group1;
+	wire[2:0] cond_group2; 
 	
 	wire[3:0] aluOp;
 	
@@ -23,11 +26,11 @@ module cpu #(parameter SIZE=16, NUMREGS=16)
 	mux2 #(SIZE) alu1(ctrlAlu1, pcOut, d1, aluIn1);
 	mux2 #(SIZE) alu2(ctrlAlu2, d2, immOut, aluIn2);
 	
-	ALU #(SIZE) alu(aluIn1, aluIn2, aluOp, carryIn, aluOut, conds, carryOut);
+	ALU #(SIZE) alu(aluOp, aluIn1, aluIn2, cIn, aluOut, cond_group1, cond_group2); //updated inputs/outputs
 	
-	register #(SIZE) psrReg(clk, reset, conds, condsOut);
+	PSR_reg PSRreg(reset, clk, en, cond_group1, cond_group2, final_group1, final_group2); // added special reg for PSR (enable signal)
 	
 	register #(SIZE) outReg(reset, clk, aluOut, aluFinal); 
 	
 	
-endmodule 
+endmodule  
