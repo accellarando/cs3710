b413ee04b8bc7a04c4230a5d2f67e054c3473271
Dana Escandor <escandordana@gmail.com>
2022-10-17 22:00:08 -0600

    added structure for test stimulus

--- registerFile_tb.v
+++ registerFile_tb.v
@@ -11,7 +11,7 @@ module registerFile_tb();
 	wire[15:0] 	ReadData1, ReadData2;
 	
 	/* Instantiate the Unit Under Test (UUT) */
-	registerFile #(.SIZE(), .REGBITS()) uut (
+	registerFile #(.SIZE(X), .REGBITS(X)) uut (
 		.clk(clk)
 		.reset(reset)
 		.writeEn(writeEn)
@@ -19,5 +19,34 @@ module registerFile_tb();
 		.srcAddr(srcAddr)
 		.dstAddr(dstAddr)
 	);
+	
+	/* Initializing inputs */
+	initial begin
+		clk		<= 1'b0;
+		reset		<= 1'b1;	// active-low reset
+		writeEn	<= 1'b0;
+		counter 	= 0;		// for switch-case block in testing different write-read values
+		#100					// wait 100 ns for global reset to finish
+	end
+	
+	/* Generate clock */
+	always #50 begin
+		clk = !clk;
+	end
+	
+	/* Adding stimulus testing */
+	always @(posedge clk) begin
+		case(counter)
+			1: begin
+			end
+			// last case: set coutner back to zero
+			default : ;
+		endcase
+		
+		counter <= counter++; // increment counter value each pos-edge of the clock to test cases subseqently
+	
+endmodule
+	
+	
 	
 	
