cb8d3e5c3e761c3db3e0362e1325a4632dab3c7a
Jacamarsh <112056604+Jacamarsh@users.noreply.github.com>
2022-10-17 22:45:06 -0600

    changed inputs/outputs of ALU, removed cIn wire

--- cpu.v
+++ cpu.v
@@ -9,7 +9,7 @@ module cpu #(parameter SIZE=16, NUMREGS=16)
 	
 	wire[SIZE-1:0] pcIn, pcOut, srcOut, dstOut, immOut, 
 		d1, d2, aluIn1, aluIn2, aluOut, shifterOut, aluOutShifted, aluFinal;
-	wire ctrlAlu1, ctrlAlu2, cIn, en; // changed carryIn to cIn, added en, removed "carryOut"
+	wire ctrlAlu1, ctrlAlu2, en; // removed cIn (no longer used in ALU)
 	
 	// changed [4:0] conds to 2 groups: [1:0] cond_group1, [2:0] cond_group2
 	wire[1:0] cond_group1;
@@ -33,7 +33,7 @@ module cpu #(parameter SIZE=16, NUMREGS=16)
 	mux2 #(SIZE) alu1(ctrlAlu1, pcOut, d1, aluIn1);
 	mux2 #(SIZE) alu2(ctrlAlu2, d2, immOut, aluIn2);
 	
-	ALU #(SIZE) alu(aluOp, aluIn1, aluIn2, cIn, aluOut, cond_group1, cond_group2, pcIn); //pcIn to output to go to pcReg
+	ALU #(SIZE) alu(aluOp, aluIn1, aluIn2, pcOut, aluOut, cond_group1, cond_group2); //pcOut to output to go to pcReg
 	
 	PSR_reg PSRreg(reset, clk, en, cond_group1, cond_group2, final_group1, final_group2); // added special reg for PSR (enable signal)
 	assign condsOut = {final_group1, final_group2};
