eba44a2667b9d9930563e46b5df9da644e3b02d7
Dana Escandor <escandordana@gmail.com>
2022-10-17 23:26:36 -0600

    testing

--- registerFile.v
+++ registerFile.v
@@ -24,30 +24,22 @@ module registerFile #(parameter SIZE = 16, REGBITS = 4) (
 	input[SIZE-1:0] 				writeData,					//	16-bit Data in
 	input[REGBITS-1:0]			srcAddr, dstAddr,			// 4-bit wide read addresses	
 	output reg[SIZE-1:0] 		readData1, readData2		// 16-bit Data out
+	
 	//	registerFile #(SIZE, REGBITS) rf(clk, reset, pcOut, aluOut, srcOut, dstOut, d1, d2);
 	);
 		
 	
 	reg [SIZE-1:0] regFile [(1<<REGBITS)-1:0]; // Declare sixteen 16-bit registers in register file 
 
-	/* Option: reading relative path*/
+	/* Reading relative path*/
 	initial begin
 	$display("Loading register file");
 	$readmemb("E:/3710/GroupProject/cs3710/reg.dat", regFile); // ! CHANGE TO YOUR LOCAL PATH !
 	$display("Done with loading register file"); 
 	end
 	
-	
-	/* Option: manually initialized */
-	/*
-	generate
-		for (genvar i = 0; i < SIZE; i++) begin
-			reg[i] = 0;
-		end
-	endgenerate
-	*/
 
-	/* assigning */
+	/* Assigning */
 	always @(posedge clk) begin
 		if(reset) begin
 			// ?? all 16 registers are set to 16'b0
@@ -57,8 +49,8 @@ module registerFile #(parameter SIZE = 16, REGBITS = 4) (
 		end
 		else begin	
 			if(writeEn) begin
-				readData1 <= regFile[srcAddr];
-				readData2 <= regFile[dstAddr];
+				readData1 <= regFile[dstAddr];
+				readData2 <= regFile[srcAddr];
 			end
 		end
 	end
--- registerFile_tb.v
+++ registerFile_tb.v
@@ -35,15 +35,24 @@ module registerFile_tb();
 	end
 	
 	/* Adding stimulus testing */
-	// reference:	readData1 uses srcAddr
-	//					readData2 uses dstAddr
+	// reference:	readData1 uses dstAddr
+	//					readData2 uses srcAddr
 	always @(posedge clk) begin
 		case(counter)
 			// ??? put #time each case -> longer time than generate clock
+			$display("\n");
 			
-			// @ 
+			// @ Writing to a register
 			1: begin
-				
+				writeEn 		<= 1;
+				dstAddr		<= 4'b1; srcAddr	<= 4'b1;	// write to register 1
+				writeData	<= 16'd2;						// write 16-bit decimal 2 to register 1
+			end
+			2: begin
+				if(readData1 == 16'd2)
+					$display("SUCCESS: write/read to register 1");
+				else
+					$display("FAILURE: write/read to register 1 -> EXPECTED = %b, ACTUAL %d", 16'd2, readData1);
 			end
 			
 			// last case: set coutner back to zero
