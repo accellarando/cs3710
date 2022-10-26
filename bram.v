adf501e2711b29d3ad40fb0bc032f0b730a12c54
Jacamarsh <112056604+Jacamarsh@users.noreply.github.com>
2022-10-26 00:39:14 -0600

    Add for using cpuWithDatapath.v - Jack

---
+++ bram.v
@@ -0,0 +1,54 @@
+// Quartus Prime Verilog Template
+// True Dual Port RAM with single clock
+
+module bram
+#(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16)
+(
+	input [(DATA_WIDTH-1):0] data_a, data_b,
+	input [(ADDR_WIDTH-1):0] addr_a, addr_b,
+	input we_a, we_b, clk,
+	output reg [(DATA_WIDTH-1):0] q_a, q_b
+);
+
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0]; // possibly switch to 0:2**ADDR_WIDTH - 1
+	integer i;
+	initial
+	begin
+//	$display("Loading memory");
+	$readmemb("<path-to-your-file>/fib-b.dat", ram); //change to correct path to .dat file
+//	$display("done loading");
+/*		for(i=0;i<ADDR_WIDTH**2;i=i+1)
+			ram[i] = 0; 
+*/
+	end
+
+	// Port A 
+	always @ (posedge clk)
+	begin
+		if (we_a) 
+		begin
+			ram[addr_a] <= data_a;
+			q_a <= data_a;
+		end
+		else 
+		begin
+			q_a <= ram[addr_a];
+		end 
+	end 
+
+	// Port B 
+	always @ (posedge clk)
+	begin
+		if (we_b) 
+		begin
+			ram[addr_b] <= data_b;
+			q_b <= data_b;
+		end
+		else 
+		begin
+			q_b <= ram[addr_b];
+		end 
+	end
+
+endmodule
--- hexTo7Seg.v
+++ hexTo7Seg.v
@@ -3,8 +3,8 @@
 // display it on a 7-segment display as a number
 // 
 module hexTo7Seg(
-		input [3:0]x,
-		output reg [6:0]z
+		input [3:0]bcd,
+		output reg [6:0] seven_seg
 		);
 
   // always @* guarantees that the circuit that is 
@@ -13,23 +13,23 @@ module hexTo7Seg(
   always @*
     // Note that the 7-segment displays on the DE1-SoC board are
     // "active low" - a 0 turns on the segment, and 1 turns it off
-    case(x)
-      4'b0000 : z = ~7'b0111111; // 0
-      4'b0001 : z = ~7'b0000110; // 1
-      4'b0010 : z = ~7'b1011011; // 2
-      4'b0011 : z = ~7'b1001111; // 3
-      4'b0100 : z = ~7'b1100110; // 4
-      4'b0101 : z = ~7'b1101101; // 5
-      4'b0110 : z = ~7'b1111101; // 6
-      4'b0111 : z = ~7'b0000111; // 7
-      4'b1000 : z = ~7'b1111111; // 8
-      4'b1001 : z = ~7'b1100111; // 9 
-      4'b1010 : z = ~7'b1110111; // A
-      4'b1011 : z = ~7'b1111100; // b
-      4'b1100 : z = ~7'b1011000; // c
-      4'b1101 : z = ~7'b1011110; // d
-      4'b1110 : z = ~7'b1111001; // E
-      4'b1111 : z = ~7'b1110001; // F
-      default : z = ~7'b0000000; // Always good to have a default! 
+    case(bcd)
+      4'b0000 : seven_seg = ~7'b0111111; // 0
+      4'b0001 : seven_seg = ~7'b0000110; // 1
+      4'b0010 : seven_seg = ~7'b1011011; // 2
+      4'b0011 : seven_seg = ~7'b1001111; // 3
+      4'b0100 : seven_seg = ~7'b1100110; // 4
+      4'b0101 : seven_seg = ~7'b1101101; // 5
+      4'b0110 : seven_seg = ~7'b1111101; // 6
+      4'b0111 : seven_seg = ~7'b0000111; // 7
+      4'b1000 : seven_seg = ~7'b1111111; // 8
+      4'b1001 : seven_seg = ~7'b1100111; // 9 
+      4'b1010 : seven_seg = ~7'b1110111; // A
+      4'b1011 : seven_seg = ~7'b1111100; // b
+      4'b1100 : seven_seg = ~7'b1011000; // c
+      4'b1101 : seven_seg = ~7'b1011110; // d
+      4'b1110 : seven_seg = ~7'b1111001; // E
+      4'b1111 : seven_seg = ~7'b1110001; // F
+      default : seven_seg = ~7'b0000000; // Always good to have a default! 
     endcase
 endmodule 
