ae5e7914a8bbe515b424852446cb7894673a7ad6
Ella Moss <ella.moss@utah.edu>
2022-10-29 16:13:27 -0600

    memory mapped io

--- bram.v
+++ bram.v
@@ -4,51 +4,72 @@
 module bram
 #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16)
 (
-	input [(DATA_WIDTH-1):0] data_a, data_b,
-	input [(ADDR_WIDTH-1):0] addr_a, addr_b,
-	input we_a, we_b, clk,
-	output reg [(DATA_WIDTH-1):0] q_a, q_b
+	input [(DATA_WIDTH-1):0] data_a, data_b, addr_a, addr_b, ex_inputs,
+	input clk, we_a, we_b,
+	output reg [(DATA_WIDTH-1):0] q_a, q_b, ex_outputs,
 );
 
 	// Declare the RAM variable
 	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0]; // possibly switch to 0:2**ADDR_WIDTH - 1
-	integer i;
-	initial
-	begin
-//	$display("Loading memory");
-	$readmemb("<path-to-your-file>/fib-b.dat", ram); //change to correct path to .dat file
-//	$display("done loading");
-/*		for(i=0;i<ADDR_WIDTH**2;i=i+1)
-			ram[i] = 0; 
-*/
+	initial begin
+		$display("Loading memory");
+		$readmemb("<path-to-your-file>/fib-b.dat", ram); //change to correct path to .dat file
 	end
 
 	// Port A 
 	always @ (posedge clk)
 	begin
-		if (we_a) 
-		begin
-			ram[addr_a] <= data_a;
-			q_a <= data_a;
+		if (we_a) begin
+			if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'xF) begin
+				//writing to external
+				ex_outputs <= data_a;
+				q_a <= ex_inputs;
+			end
+			else begin
+				ram[addr_a] <= data_a;
+				q_a <= data_a;
+			end
 		end
-		else 
-		begin
-			q_a <= ram[addr_a];
+		else begin
+			if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'xF)
+				q_a <= ex_inputs;
+			else
+				q_a <= ram[addr_a];
 		end 
 	end 
 
 	// Port B 
 	always @ (posedge clk)
 	begin
-		if (we_b) 
-		begin
+		if (we_b) begin
+			if(addr_b[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'xF) begin
+				//writing to external
+				ex_outputs <= data_b;
+				q_b <= ex_inputs;
+			end
+			else begin
+				ram[addr_b] <= data_b;
+				q_b <= data_b;
+			end
+		end
+		else begin
+			if(addr_b[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'xF)
+				q_b <= ex_inputs;
+			else
+				q_b <= ram[addr_b];
+		end 
+	end 
+	/*
+	always @ (posedge clk)
+	begin
+		if (we_b) begin
 			ram[addr_b] <= data_b;
 			q_b <= data_b;
 		end
-		else 
-		begin
+		else begin
 			q_b <= ram[addr_b];
 		end 
 	end
+	*/
 
 endmodule
