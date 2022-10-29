c38458d6f95af606b2f05c1718b6a8fc81fd95a7
Dana Escandor <escandordana@gmail.com>
2022-10-29 15:42:20 -0600

    ideas for datapath

--- datapath.v
+++ datapath.v
@@ -1,2 +1,24 @@
 module datapath #(parameter SIZE = 16) (
+//	POSSIBLE INPUTS TO CONSIDER
+//	-immediate or regfile's dataout
+//		+(immediate) zeroextend/signextend
+//	-select output for whatever alu/shfiting/store or copy to mem
+//	-set flags
+//		+flag for writing to mem, store instr in reg, left shift immediate, get new instr from mem
+//	-alu opcodes
+//	-enable register to write
+//	-program counter
+//		+instr if dataout or pc
+//		+pc increment, next pc is output
+//	
+//	-memory access for ports (outside of datapath)
+//		+write data, data addr, i/o input data, enable write(?)
+		
+	
+	
+	/* Outputs */
+//	POSSIBLE OUTPUTS TO CONSIDER
+//	-current instruction from mem
+//	-program counter
+//	-current flag set
 	);
