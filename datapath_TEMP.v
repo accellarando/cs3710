dd86dbafdfdfb4a95b229d526311291b25cfd1ed
Dana Escandor <escandordana@gmail.com>
2022-10-30 19:46:48 -0600

    added test bench for statemachine not finished

--- datapath_TEMP.v
+++ datapath_TEMP.v
@@ -21,4 +21,57 @@
 //	-current flag set
 
 module datapath_TEMP #(parameter SIZE = 16) (
+	/* INPUTS* */
+	input
+	
+	/* OUTPUTS */
+	output reg[SIZE-1:0] PC
 	);
+	
+	/* INSTANTIATE MODULES */
+	registerFile regfile (
+		.clk(clk),
+		.reset(reset),
+		.writeEn(writeEn),
+		.writeData(writeData),
+		.srcAddr(srcAddr),
+		.dstAddr(dstAddr),
+		.readData1(readData1),
+		.readData2(readData2)
+	);
+	
+	/* PROGRAM COUNTER */
+//	PC as a loadable counter: 
+//	-Load the counter for the update and displacement functions (muxes) and count the counter for the increment-the-pc function
+//	-[advantage] May not have to use the complete datapath for each PC increment
+
+//	Datapath PC operations:
+//	-PC needs to be incremented by one word of 16-bits (the normal case)
+//	-Added to a sign-extended displacement (for branches)
+//	-Loaded from a register (for jumps)
+//	-JAL instr -> PC needs a path to regfile so that PC+1 (addr of the next instr following the JAL) can be stored in the link register, no reads
+
+// Consider possible PC issues:
+//	-[un/signed arithmetic] Unsigned PC that is operated on by a two's complement ALU
+//		+unsigned PC can directly addr 0 - 64k locations (each location is a 16-bit word) == 64k words in the addr space w/ each word being 16-bits
+//		+(subtraction) unsigned # w/ MSB of 1 binary added to a signed # w/ MSB of 1 -> overflow
+	
+//	[mini mips] flopenr    #(WIDTH)  pcreg(clk, reset, pcen, nextpc, pc);
+
+
+	// Immediate zero-extend or sign-extend
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
---
+++ stateMachine_tb.v
@@ -0,0 +1,1 @@
+module stateMachine_tb();
