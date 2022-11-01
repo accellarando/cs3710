f04ec5f207d4acb0a8799e804f10e2d97fb94c9b
Dana Escandor <escandordana@gmail.com>
2022-10-31 19:45:27 -0600

    added cpuWithDatapathTb/memoryTb from main to my branch

---
+++ cpuWithDatapathTb.v
@@ -0,0 +1,25 @@
+//OLD NAME: memoryTb.v
+module cpuWithDatapathTb();
+
+initial begin
+	reset = 1'b1;
+	nextStateButton = 1'b1;
+	#200;
+	reset = 1'b0;
+	#200;
+	reset = 1'b1;
+	#200;
+	nextStateButton = 1'b0;
+	#200;
+	nextStateButton = 1'b1;
+	//check that the outputs are correct
+	$display("Read data: %b, should be ...",readData);
+	$display("Write data: %b, should be ...",writeData);
+	#200;
+	nextStateButton = 1'b0;
+	#200;
+	nextStateButton = 1'b1;
+	$display("...");
+end
+
+endmodule 
