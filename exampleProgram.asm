ca1ba577151b3aae75277671760c7246cbbdf532
Ella Moss <ella.moss@utah.edu>
2022-11-21 18:01:52 -0700

    assembly changes

--- exampleProgram.asm
+++ exampleProgram.asm
@@ -1,12 +1,14 @@
 .text
 #Load data from memory into regfile
-#0x100
-MOVI $30 %r2
+#$1025 = 0b 100 00000001
+LUI $8 %r2
+ORI $1 %r2
 LOAD %r1 %r2
-#Set flags - load something into memory address $30 s.t. this will overflow!
+#Set flags - load something into memory address $1025 s.t. this will overflow!
 ADDI $1 %r1
-#Store result into memory
-MOVI $31 %r2
+#Store result into memory at 1026
+LUI $8 %r2
+ORI $2 %r2
 STOR %r1 %r2
 #Reload result into regfile
 LUI $127 %r1
@@ -20,8 +22,10 @@ ORI $127 %r1
 	CMP %r1 %r2
 	BNE $-4
 #Write result into memory
-MOVI $32 %r2
+LUI $8 %r2
+ORI $3 %r3
 STOR %r2 %r1
 #Display result at output
-MOVI $62 %r2
+LUI $127 %r2
+ORI $127 %r2
 STOR %r1 %r2
