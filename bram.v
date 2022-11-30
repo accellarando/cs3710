fec6cf820b27967988d2f2b50aaa1c498da327c9
Jacamarsh <112056604+Jacamarsh@users.noreply.github.com>
2022-11-30 14:32:40 -0700

    BRAM Update 11/30
    Latches found, unused logic (if, else, else block).
    Data is handled correctly, only problem could be in 'initial begin' when loading from path to .dat file, where bram needs to be initialized to all zeros, or given initial values from another file/module to read from. This is unlikely though.

--- bram.v
+++ bram.v
@@ -4,9 +4,10 @@
 module bram
 #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16)
 (
-	input [(DATA_WIDTH-1):0] data_a, data_b, addr_a, addr_b, ex_inputs,
+	input [(DATA_WIDTH-1):0] data_a, data_b, addr_a, addr_b,
 	input clk, we_a, we_b,
-	output reg [(DATA_WIDTH-1):0] q_a, q_b, ex_outputs
+	output reg [(DATA_WIDTH-1):0] q_a, q_b
+	// removed ex_outputs and ex_inputs
 );
 
 	// Declare the RAM variable
@@ -18,49 +19,66 @@ module bram
 		$display("Done.");
 	end
 
+	
+	
+	
+	// ** UPDATE: If not working, only thing would be an initial begin block to initialize
+	//		BRAM to all zeros or with initial values. ***
+	
+	
+	
+	
+	
+	
+	// *** BRAM problem: in always block below: a if, else, else statement. Logic never seen.
+	
+	
 	// Port A 
 	always @ (posedge clk)
 	begin
 		if (we_a) begin
-			if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF) begin
-				//writing to external
-				ex_outputs <= data_a; //this was yelling at us, idk dude
-				q_a <= ex_inputs;
-			end
-			else begin
-				ram[addr_a] <= data_a;
-				q_a <= data_a;
-			end
+		// removed if statement:  if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF) begin
+
+			//writing to external
+			ram[addr_a] <= data_a; //this was yelling at us, idk dude
+			q_a <= data_a;
 		end
 		else begin
-			if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF)
-				q_a <= ex_inputs;
-			else
 				q_a <= ram[addr_a];
-		end 
+		end
+//		else begin
+//			//// removed if statement:  if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF) begin
+//			if(addr_a[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF)
+//				q_a <= ex_inputs;
+//			else
+//				q_a <= ram[addr_a];
+//		end 
 	end 
 
 	// Port B 
 	always @ (posedge clk)
 	begin
 		if (we_b) begin
-			if(addr_b[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF) begin
+//			if(addr_b[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF) begin
 				//writing to external
 				//ex_outputs <= data_b;
-				q_b <= ex_inputs;
-			end
-			else begin
+//				q_b <= ex_inputs;
+//			end
+//			else begin
 				ram[addr_b] <= data_b;
 				q_b <= data_b;
-			end
+//			end
 		end
 		else begin
-			if(addr_b[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF)
-				q_b <= ex_inputs;
-			else
+//			if(addr_b[(ADDR_WIDTH-1):(ADDR_WIDTH-4)] == 4'hF)
+//				q_b <= ex_inputs;
+//			else
 				q_b <= ram[addr_b];
 		end 
 	end 
+	
+	
+	
 	/*
 	always @ (posedge clk)
 	begin
