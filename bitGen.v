742e819bd1e7bbf3494448b9c5974ae3e2d08b43
Ella Moss <ella.moss@utah.edu>
2022-12-03 23:47:47 -0700

    loaded glyphs into memory

--- bitGen.v
+++ bitGen.v
@@ -9,7 +9,7 @@ module bitGen(
 	output reg[15:0] memAddr,
 	output reg[23:0] rgb);
 
-parameter BG_COLOR = ~24'hFFFFFF;
+parameter BG_COLOR = 16'b1111111111111111;
 
 parameter TOP = 160;
 parameter BOTTOM = 320;
@@ -33,6 +33,7 @@ reg[9:0] j;
 reg[3:0] k; //keeps track of how many pixels in we are (read)
 reg[9:0] oldHc;
 reg[9:0] oldVc;
+reg isGlyph;
 
 parameter FETCH_PIX = 4'd0;
 parameter WRITE_PIX = 4'd1;
@@ -79,25 +80,26 @@ always@(posedge clk) begin
 		FETCH_HUN: memAddr <= count_addr + 2'd2;
 		FETCH_HUN_WB: begin
 			memAddr <= count_addr + 2'd2;
-			digitHun <= memData;
+			digitHun <= memData[3:0];
 		end
 		
 		FETCH_TEN: memAddr <= count_addr + 2'd2;
 		FETCH_TEN_WB: begin
 			memAddr <= count_addr + 2'd2;
-			digitTen <= memData;
+			digitTen <= memData[3:0];
 		end
 
 		FETCH_ONE: memAddr <= count_addr + 2'd2;
 		FETCH_ONE_WB: begin
 			memAddr <= count_addr + 2'd2;
-			digitOne <= memData;
+			digitOne <= memData[3:0];
 		end
 
 		FETCH_PIX: begin
-			i <= 4'b0;
+			i <= 4'd15; //-1
 			if(vCount >= TOP && vCount <= BOTTOM) begin
 				if(hCount >= HUN_START && hCount <= HUN_END) begin
+					isGlyph <= 1'b1;
 					if(hCount == HUN_START) begin
 						j <= 10'b0;
 						k <= 4'b0;
@@ -115,6 +117,7 @@ always@(posedge clk) begin
 					pixels <= memData;
 				end
 				else if(hCount >= TEN_START && hCount <= TEN_END) begin
+					isGlyph <= 1'b1;
 					if(hCount == TEN_START) begin
 						j <= 10'b0;
 						k <= 4'b0;
@@ -131,6 +134,7 @@ always@(posedge clk) begin
 					pixels <= memData;
 				end
 				else if(hCount >= ONE_START && hCount <= ONE_END) begin
+					isGlyph <= 1'b1;
 					if(hCount == ONE_START) begin
 						j <= 10'b0;
 						k <= 4'b0;
@@ -146,25 +150,31 @@ always@(posedge clk) begin
 						+ j;
 					pixels <= memData;
 				end
-				else
+				else begin
+					isGlyph <= 1'b0;
 					pixels <= BG_COLOR;
+				end
 			end
-			else
+			else begin
+				isGlyph <= 1'b0;
 				pixels <= BG_COLOR;
+			end
 		end
 
 		WRITE_PIX: begin
+			if(isGlyph)
+				pixels <= memData;
 			if(oldHc != hCount) begin
 				//update...
 				i <= i+1'b1;
 				oldHc <= hCount;
 			end
-			rgb <= ~{ {4'd8{pixels[3'd3 * i + 3'd3]}},
+			rgb <= { {4'd8{pixels[3'd3 * i + 3'd3]}},
 				{4'd8{pixels[3'd3 * i + 3'd2]}},
 				{4'd8{pixels[3'd3 * i + 3'd1]}}};
 			// j <= j+1'b1;
 		end
-		default: memAddr <= glyphs;
+		default: ;
 	endcase
 end
 endmodule 
