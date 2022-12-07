9e9b2d24f5c5691ec2fc06d32978b33cdac01078
Ella Moss <ella.moss@utah.edu>
2022-12-07 01:29:05 -0700

    changed branches to jumps

--- ass/program.asm
+++ ass/program.asm
@@ -11,7 +11,9 @@
 #    0004: beam B interrupted - wait for beam A -> go to state 5 on A
 #    0005: beam A interrupted - wait for beams to reset -> go to state 6 once both are active
 #    0006: decrement counter -> proceed immediately to state 0
+#
 #    what to do if only one beam is triggered?? will that happen?
+#    trigger reset button?
 #r6: value of gpio pins
 #r7: current ones place
 #r8: current tens place
@@ -27,20 +29,33 @@
 LOAD %r6 %r1
 
 #Check state, jump to different instruction depending on state
+MOVI .zero %r10
 CMPI $0 %r5
-BEQ .zero
+JEQ %r10
+
+MOVI .one %r10
 CMPI $1 %r5
-BEQ .one
+JEQ %r10
+
+MOVI .two %r10
 CMPI $2 %r5
-BEQ .two
+JEQ %r10
+
+MOVI .three %r10
 CMPI $3 %r5
-BEQ .three
+JEQ %r10
+
+MOVI .four %r10
 CMPI $4 %r5
-BEQ .four
+JEQ %r10
+
+MOVI .five %r10
 CMPI $5 %r5
-BEQ .five
+JEQ %r10
+
+MOVI .six %r10
 CMPI $6 %r5
-BEQ .six
+JEQ %r10
 
 .zero
 #if A triggered, change state to 1
@@ -50,43 +65,42 @@ CMPI $1 %r1
 #check this branch value...
 BNE $3
 MOVI $1 %r5
-BUC .main
+MOVI .main %r10
+JUC %r10
 
 #get B - could move this to a function call but idk if it's worth it tbh
-LUI $-1 %r10
-ORI $-1 %r10
-LSH %r10 %r1
 #if B triggered, change state to 4
-CMPI $1 %r1
+ANDI $2 %r1
+CMPI $2 %r1
 #check this branch amt
-BNE $2
+BNE $3
 MOVI $4 %r5
 
 #loop
-BUC .main
+MOVI .main %r10
+JUC %r10
 
 .one
 #if B triggered, change state to 2
-LUI $-1 %r10
-ORI $-1 %r10
-LSH %r10 %r1
-ANDI $1 %r1
-CMPI $1 %r1
-BNE $2
+ANDI $2 %r1
+CMPI $2 %r1
+BNE $3
 MOVI $2 %r5
 
 #loop
-BUC .main
+MOVI .main %r10
+JUC %r10
 
 .two
 #if a and b reset, ie ab==00, change state to 3
 ANDI $3 %r1
 CMPI $0 %r1
-BNE .main
+MOVI .main %r10
+JNE %r10
 MOVI $3 %r5
 
 #loop
-BUC .main
+JUC %r10
 
 .three
 #load from memory
@@ -99,19 +113,20 @@ ADDI $1 %r7
 
 #check ones place carry
 CMPI $10 %r7
-BNE .threeUpdate
+MOVI .threeUpdate %r10
+JNE %r10
 MOVI $0 %r7
 ADDI $1 %r8
 
 #check tens place carry
 CMPI $10 %r8
-BNE .threeUpdate
+JNE %r10
 MOVI $0 %r8
 ADDI $1 %r9
 
 #check hundreds place carry
 CMPI $10 %r9
-BNE .threeUpdate
+JNE %r10
 MOVI $0 %r9
 
 .threeUpdate
@@ -120,7 +135,8 @@ STOR %r8 %r3
 STOR %r9 %r4
 
 #loop
-BUC .main
+MOVI .main %r10
+JUC %r10
 
 .four
 #if A triggered, change state to 5
