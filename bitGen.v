131f9b30e9198258da0588aefd869de94fb4b559
Ella Moss <ella.moss@utah.edu>
2022-12-07 19:58:43 -0700

    noooo

--- bitGen.v
+++ bitGen.v
@@ -9,30 +9,21 @@ module bitGen(
 	output reg[15:0] memAddr,
 	output reg[23:0] rgb);
 
-parameter BG_PIXELS = ~16'hFFFF;
+parameter BG_COLOR = ~16'b1111111111111111;
 
-parameter TOP = 176;
-parameter BOTTOM = 304;
-parameter HUN_START = 64;
-parameter HUN_END = 192;
-parameter TEN_START = 208;
-parameter TEN_END = 336;
-parameter ONE_START = 352;
-parameter ONE_END = 480;
-
-parameter FETCH_NEXT_LINE = 4'd0;
-parameter FETCH_NEXT_LINE_WB = 4'd1;
-parameter FETCH_HUN = 4'd2;
-parameter FETCH_HUN_WB = 4'd3;
-parameter FETCH_TEN = 4'd4;
-parameter FETCH_TEN_WB = 4'd5;
-parameter FETCH_ONE = 4'd6;
-parameter FETCH_ONE_WB = 4'd7;
-parameter WRITE_TO_SCREEN = 4'd8;
+parameter TOP = 160;
+parameter BOTTOM = 320;
+parameter HUN_START = 20;
+parameter HUN_END = 180;
+parameter TEN_START = 200;
+parameter TEN_END = 360;
+parameter ONE_START = 380;
+parameter ONE_END = 540;
 
 reg[3:0] thisState, nextState;
 reg[2:0] pixelCounter;
 reg[3:0] digitOne, digitTen, digitHun;
+reg[3:0] digit;
 reg[9:0] start;
 reg[15:0] pixelAddr;
 reg[15:0] pixels;
@@ -42,9 +33,17 @@ reg[9:0] j;
 reg[4:0] k; //keeps track of how many pixels in we are (read)
 reg[9:0] oldHc;
 reg[9:0] oldVc;
-reg[1919:0] thisLine;
-reg[1919:0] nextLine;
 reg isGlyph;
+
+parameter FETCH_PIX = 4'd0;
+parameter WRITE_PIX = 4'd1;
+parameter FETCH_HUN = 4'd2;
+parameter FETCH_HUN_WB = 4'd3;
+parameter FETCH_TEN = 4'd4;
+parameter FETCH_TEN_WB = 4'd5;
+parameter FETCH_ONE = 4'd6;
+parameter FETCH_ONE_WB = 4'd7;
+
 always@(posedge clk) begin
 	thisState <= nextState;
 end
@@ -60,29 +59,23 @@ always@(*) begin
 		FETCH_TEN: nextState <= FETCH_TEN_WB;
 		FETCH_TEN_WB: nextState <= FETCH_ONE;
 		FETCH_ONE: nextState <= FETCH_ONE_WB;
-		FETCH_ONE_WB:
-			if(!hSync)
-				nextState <= FETCH_NEXT_LINE;
+		FETCH_ONE_WB: nextState <= FETCH_PIX;
+		FETCH_PIX: nextState <= WRITE_PIX;
+		WRITE_PIX:
+			if(!vSync)
+				nextState <= FETCH_HUN;
+			else if(i >= 3'd4)
+				nextState <= FETCH_PIX;
 			else
-				nextState <= WRITE_TO_SCREEN;
-		FETCH_NEXT_LINE: nextState <= FETCH_NEXT_LINE_WB;
-		FETCH_NEXT_LINE_WB:
-			if(i>0)
-				nextState <= FETCH_NEXT_LINE;
-			else
-				nextState <= WRITE_TO_SCREEN;
-		WRITE_TO_SCREEN:
-			if(!hSync)
-				nextState <= FETCH_NEXT_LINE;
-			else
-				nextState <= WRITE_TO_SCREEN;
+				nextState <= WRITE_PIX;
 	endcase
 end
 
-always@(posedge hSync)
-	thisLine <= nextLine;
-
 always@(posedge clk) begin
+	if(!reset) begin
+		i <= 4'b0;
+		j <= 10'b0;
+	end
 	case(thisState)
 		FETCH_HUN: memAddr <= count_addr + 2'd2;
 		FETCH_HUN_WB: begin
@@ -100,68 +93,88 @@ always@(posedge clk) begin
 		FETCH_ONE_WB: begin
 			memAddr <= count_addr + 2'd0;
 			digitOne <= memData[3:0];
-			if(!hSync)
-				i <= 1919;
 		end
 
-		FETCH_NEXT_LINE: begin
-			/*
-			base glyph address is just 1024*glyph, ie glyph<<10
-			row is (row/4)*32, ie, row*8, ie row<<3
-			col is col/4, ie row>>2
-			*/
-			//figure out which region we're in
+		FETCH_PIX: begin
+			i <= 4'd15; //-1
 			if(vCount >= TOP && vCount <= BOTTOM) begin
 				if(hCount >= HUN_START && hCount <= HUN_END) begin
 					isGlyph <= 1'b1;
-					memAddr <= digitHun << 10 
-						+ (vCount-TOP) << 3
-						+ (hCount - HUN_START) >> 2;
+					if(hCount == HUN_START) begin
+						j <= 10'b0;
+						k <= 5'b0;
+					end
+					else begin
+						if(k==4'd4)
+							j <= j+1'b1;
+						else
+							k <= k + 1'b1;
+					end
+					//in the hundreds region
+					memAddr <= glyphs + (digitHun << 4'd12) + (digitHun << 4'd10) //base glyph addr
+						+ (vCount - TOP) * (2'd2 << 5) //row
+						+ j;
+					pixels <= memData;
 				end
 				else if(hCount >= TEN_START && hCount <= TEN_END) begin
 					isGlyph <= 1'b1;
-					memAddr <= digitTen << 10
-						+ (vCount - TOP) << 3
-						+ (hCount - HUN_START) >> 2;
+					if(hCount == TEN_START) begin
+						j <= 10'b0;
+						k <= 4'b0;
+					end
+					else begin
+						if(k==4'd4)
+							j <= j+1'b1;
+						else
+							k <= k + 1'b1;
+					end
+					memAddr <= glyphs + (digitTen << 4'd12) + (digitTen << 4'd10) //base glyph addr
+						+ (vCount - TOP) * (2'd2 << 5) //row
+						+ j;
+					pixels <= memData;
 				end
 				else if(hCount >= ONE_START && hCount <= ONE_END) begin
 					isGlyph <= 1'b1;
-					memAddr <= digitOne << 10
-						+ (vCount - TOP) << 3
-						+ (hCount - HUN_START) >> 2;
+					if(hCount == ONE_START) begin
+						j <= 10'b0;
+						k <= 4'b0;
+					end
+					else begin
+						if(k==4'd4)
+							j <= j+1'b1;
+						else
+							k <= k + 1'b1;
+					end
+					memAddr <= glyphs + (digitOne << 4'd12) + (digitOne << 4'd10) //base glyph addr
+						+ (vCount - TOP) * (2'd2 << 5) //row
+						+ j;
+					pixels <= memData;
 				end
 				else begin
 					isGlyph <= 1'b0;
-					memAddr <= 16'hFFFF;
+					pixels <= BG_COLOR;
 				end
 			end
-			else
+			else begin
 				isGlyph <= 1'b0;
+				pixels <= BG_COLOR;
+			end
 		end
-		FETCH_NEXT_LINE_WB: begin
-			if(!isGlyph)
-				pixels <= BG_PIXELS;
-			else begin
+
+		WRITE_PIX: begin
+			if(isGlyph)
 				pixels <= memData;
+			if(oldHc != hCount) begin
+				//update...
+				i <= i+1'b1;
+				oldHc <= hCount;
 			end
-			nextLine[i -: 16] = { {{3'd4}{pixels[15:13]}},
-				{{3'd4}{pixels[11:9]}},
-				{{3'd4}{pixels[7:5]}},
-				{{3'd4}{pixels[3:1]}}};
-			i <= i-5'd16;
+			rgb <= { {4'd8{pixels[3'd3 * i + 3'd3]}},
+				{4'd8{pixels[3'd3 * i + 3'd2]}},
+				{4'd8{pixels[3'd3 * i + 3'd1]}}};
+			// j <= j+1'b1;
 		end
-		WRITE_TO_SCREEN: begin
-			if(i<3) begin
-				i <= 1919;
-				rgb <= {BG_PIXELS,BG_PIXELS[7:0]};
-			end
-			else begin
-				rgb <= { {{4'd8}{thisLine[i]}},
-					{{4'd8}{thisLine[i-1]}},
-					{{4'd8}{thisLine[i-2]}}};
-				i <= i-3'd3;
-			end
-		end
+		default: ;
 	endcase
 end
 endmodule 
